0dd600a138f80c64ca6ba01ff260f30c63c61d10
Ivan <santivan27@hotmail.com>
2018-06-06 11:26:22 -0500

    commit nuevo

---
+++ ejecuta.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+######################### Ejecuta java    ###################################
+###################### Ivan Santiago Jorge ##################################
+
+cd /home/servsoc/workspace/Suma/src/suma
+javac SumaClase.java;
+java SumaClase;
+javac SumaClaseTest.java;
+java SumaClaseTest
